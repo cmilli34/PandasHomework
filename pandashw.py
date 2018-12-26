5e54f3c26cb7ee5720c1f4ab8eb076d12b310b4b
Elon Phoenix <carolinemiller@fastmail.fm>
2018-12-26 18:55:10 -0500

    yup

---
+++ pandashw.py
@@ -0,0 +1,10 @@
+#Pandas homework
+#put this in jupyter ok
+#now let's get started
+#look @ jupyter for instructions 
+#path: Users/carolinemiller/Documents/github/PandasHomework pandashw.py
+
+import pandas as pd
+import numpy as np
+
+purchase_data = pd.read_csv("purchase_data2.csv")
